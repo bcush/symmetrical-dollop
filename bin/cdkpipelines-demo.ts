f243eaadfccfc37de9d45e3fea29d67c7f1a233c
Brian Cushman <me@bcu.sh>
2021-12-22 14:27:22 -0800

    add dev

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import { App } from '@aws-cdk/core';
 import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+import { CdkpipelinesDemoStage } from '../lib/cdkpipelines-demo-stage';
 
 const app = new App();
 
 new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
   env: { account: '560942504039', region: 'us-east-1' },
+});
+
+new CdkpipelinesDemoStage(app, 'Dev', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 });
 
 app.synth();
